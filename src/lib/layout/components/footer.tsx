4d4d5b10f95e56917af893653a9f97ae6230b889
Jorge Garcia <jorgerenemg@icloud.com>
2024-12-21 01:57:44 -0600

    feat(components): added 404 page, removed unused images and removed header, updated footer

--- src/lib/layout/components/footer.tsx
+++ src/lib/layout/components/footer.tsx
@@ -1,11 +1,19 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import { ColorModeButton } from '@/components/ui/color-mode';
+
 export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <Flex as="footer" width="full" justifyContent="center">
-      <Box marginLeft="auto">
-        <ColorModeButton />
-      </Box>
+    <Flex
+      as="footer"
+      width="full"
+      justifyContent="space-between"
+      alignItems="center"
+      padding={4}
+    >
+      <Text fontSize="sm">&copy; {currentYear} - Braintegral</Text>
+      <ColorModeButton />
     </Flex>
   );
 };
--- src/lib/pages/404/index.tsx
+++ src/lib/pages/404/index.tsx
@@ -1,46 +1,24 @@
 import { Button } from '@/components/ui/button';
-import {
-  Box,
-  Heading,
-  Image,
-  Text,
-  Link as ChakraLink,
-  Flex,
-} from '@chakra-ui/react';
-import Link from 'next/link';
+import { Box, Heading, Flex, Link as ChakraLink } from '@chakra-ui/react';
 
 export const Page404 = () => {
   return (
-    <Flex minHeight="70vh" direction="column" justifyContent="center">
-      <Image
-        margin="0 auto"
-        width={{ base: '100%', sm: '70%', md: '60%' }}
-        src="/404 Error-pana.svg"
-        alt="Error 404 not found Illustration"
-      />
-      <Text textAlign="center" fontSize="xs" color="gray">
-        <ChakraLink
-          href="https://stories.freepik.com/web"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Illustration by Freepik Stories
-        </ChakraLink>
-      </Text>
-
-      <Box marginY={4}>
-        <Heading textAlign="center" size="lg">
-          Page not Found.
-        </Heading>
-
-        <Box textAlign="center" marginTop={4}>
-          <Text fontSize="sm" color="gray">
-            It&apos;s Okay!
-          </Text>
-          <Button asChild size="sm">
-            <Link href="/">Let&apos;s Head Back</Link>
-          </Button>
-        </Box>
+    <Flex
+      minHeight="70vh"
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Heading size="7xl" textAlign="center">
+        404
+      </Heading>
+      <Heading size="md" textAlign="center" marginTop={2}>
+        Página no encontrada
+      </Heading>
+      <Box marginTop={6}>
+        <Button asChild size="md">
+          <ChakraLink href="/">Regresar</ChakraLink>
+        </Button>
       </Box>
     </Flex>
   );
